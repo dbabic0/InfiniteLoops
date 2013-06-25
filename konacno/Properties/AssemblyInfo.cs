3203f84c16ea6a299bcd8154384ddd8e43af021f
Iva Sodić <isodic@foi.hr>
2013-06-25 19:03:02 +0200

    aplikacija kraj

--- konacno/Properties/AssemblyInfo.cs
+++ konacno/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Forme")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Eimenik")]
+[assembly: AssemblyDescription("Software imenik")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Forme")]
+[assembly: AssemblyCompany("InfinityLoops")]
+[assembly: AssemblyProduct("Eimenik imenik")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
